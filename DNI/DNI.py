f068a3531cc48448784010ef5abb69bcf306643b
Mario Aguirre <maguirre@eepmad.com>
2021-11-24 20:36:35 +0100

    24/11/2021 - CASA

--- DNI/DNI.py
+++ DNI/DNI.py
@@ -1,35 +1,40 @@
 class DNI:
-    def __init__(self, dni, letra):
-        self.dni = dni
+    # CONTRUCTOR VACIO.
+    def __init__(self):
+        self.numero
+        self.letra
+
+    # CONTRUCTOR CON PARAMETROS.
+    def __init__(self, numero, letra):
+        self.numero = numero
         self.letra = letra
-        print(dni, letra)
 
-    # metodo get
-    def get_dni(self, dni):
-        return self.dni
+    # METODOS
+    def get_dni(self, numero):
+        return self.numero
 
-    # metodo set
-    def set_dni(self, dni):
-        self.dni = dni
+    def set_dni(self, numero):
+        self.numero = numero
 
     def leer(self):
-        print("Numero del DNI")
-        numeroDNI = int(input())
-        self.dni= numeroDNI
+        print("Numero del DNI: ", end='')
+        numero = int(input())
+        self.numero = numero
 
-    def mostrar(self):
-        print("El DNI es ", self.dni, "-", self.letra)
+    def mostrarDNI(self):
+        self.calcular()
+        print("El DNI es: ", self.numero, "-", self.letra)
 
     def calcular(self):
-        tablaLetras = {0: "T", 1: "R", 2: "W", 3: "A", 4: "G", 5: "M", 6: "Y", 7: "F", 8: "P", 9: "D", 10: "X", 11: "B",
-                       12: "N", 13: "J", 14: "Z", 15: "S", 16: "Q", 17: "V", 18: "H", 19: "L", 20: "C", 21: "K", 22: "E"}
+        letras = tablaLetras = {0: "T", 1: "R", 2: "W", 3: "A", 4: "G", 5: "M", 6: "Y", 7: "F", 8: "P", 9: "D", 10: "X",
+                                11: "B", 12: "N", 13: "J", 14: "Z", 15: "S", 16: "Q", 17: "V", 18: "H", 19: "L",
+                                20: "C", 21: "K", 22: "E"}
+        resto = self.numero % 23
+        letra = tablaLetras[resto]
+        self.letra = letra
 
-        resto = self.dni % 23
-        print(resto)
-        letraTablaDNI = tablaLetras[resto]
-        self.letra = letraTablaDNI
 
-dni1 = DNI(70, 7)
-dni1.leer()
-dni1.calcular()
-dni1.mostrar()
+# COMIENZO DE PRUEBAS.
+dni = DNI(1, 2)
+dni.leer()
+dni.mostrarDNI()
